7097266e4857c6ee15807f01019a4ae0bf331886
thisisankit27 <thisisankitsrivastava@gmail.com>
2022-11-20 11:54:11 +0530

    Header Completed

--- src/components/DisabilityHelp.jsx
+++ src/components/DisabilityHelp.jsx
@@ -2,6 +2,8 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import imgTranslate from '../images/Vector.png';
+import { MdOutlineFormatSize } from 'react-icons/md';
 
 const DisabilityHelp = () => {
     return (
@@ -15,14 +17,19 @@ const DisabilityHelp = () => {
                     </Nav>
                     {/*It is responsible for the other nav bar content moving to the right.*/}
                     <Nav variant="tabs" defaultActiveKey="#action4" className="my-2 my-lg-0" style={{ maxHeight: '100px' }}>
-                        <NavDropdown title="Language" id="navbarScrollingDropdown">
+                        <NavDropdown title={<img
+                            src={imgTranslate}
+                            alt="user pic"
+                        />} id="navbarScrollingDropdown">
+                            {/* <NavDropdown title="Language" id="navbarScrollingDropdown"> */}
                             <NavDropdown.Item href="#action1">English</NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item href="#action2">हिन्दी</NavDropdown.Item>
+
                         </NavDropdown>
 
                         <Nav.Link href="#action3">Screen Reader Access</Nav.Link>
-                        <Nav.Link href="#action4">Font Size</Nav.Link>
+                        <Nav.Link href="#action4"><MdOutlineFormatSize />Font Size</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container >
